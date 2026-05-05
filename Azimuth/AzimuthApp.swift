c8c4817dff2f3c5cf7ec11a66bc64ffd22a038af
gsdv <sdvgab@gmail.com>
2026-05-05 10:27:20 -0500

    Initial Commit

---
+++ Azimuth/AzimuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzimuthApp.swift
+//  Azimuth
+//
+//  Created by Gabriele Scotto di Vettimo on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct AzimuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
